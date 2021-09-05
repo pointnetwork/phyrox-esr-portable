e1e52cc3423346b3fc8cd088ca3acc50308250ce
Amaury Hernandez-Aguila <amherag@gmail.com>
2021-09-04 21:38:42 -0700

    Vendoring dependencies.

---
+++ vendor/github.com/portapps/portapps/v3/pkg/utl/electron.go
@@ -0,0 +1,21 @@
+package utl
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// FindElectronAppFolder returns the app electron folder
+func FindElectronAppFolder(prefix string, source string) (string, error) {
+	rootFiles, _ := ioutil.ReadDir(source)
+
+	for _, f := range rootFiles {
+		if strings.HasPrefix(f.Name(), prefix) && f.IsDir() {
+			return f.Name(), nil
+		}
+	}
+
+	return "", errors.Errorf("Electron main path does not exist with prefix '%s' in %s", prefix, source)
+}
